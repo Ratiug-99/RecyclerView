2069847d5529892789c533db3fa4e8a0c7e3c834
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-09-17 18:44:08 +0300

    create empty adapter and ViewHolder

---
+++ app/src/main/java/com/ratiug/dev/courrsera_recyclerview/Adapter/myViewHolder.java
@@ -0,0 +1,12 @@
+package com.ratiug.dev.courrsera_recyclerview.Adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class myViewHolder extends RecyclerView.ViewHolder {
+    public myViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
