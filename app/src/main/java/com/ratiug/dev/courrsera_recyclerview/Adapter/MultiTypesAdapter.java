ef7382b10dd05edf04b883d679657ecfd49b7263
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-09-22 11:08:23 +0300

    add remove onCLick

--- app/src/main/java/com/ratiug/dev/courrsera_recyclerview/Adapter/MultiTypesAdapter.java
+++ app/src/main/java/com/ratiug/dev/courrsera_recyclerview/Adapter/MultiTypesAdapter.java
@@ -2,20 +2,25 @@ package com.ratiug.dev.courrsera_recyclerview.Adapter;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ratiug.dev.courrsera_recyclerview.ImageRowType;
+import com.ratiug.dev.courrsera_recyclerview.MainActivity;
 import com.ratiug.dev.courrsera_recyclerview.MessageRowType;
 import com.ratiug.dev.courrsera_recyclerview.R;
 import com.ratiug.dev.courrsera_recyclerview.RowType;
 
 import java.util.List;
 import java.util.Random;
+
+import static android.content.ContentValues.TAG;
 
 public class MultiTypesAdapter extends RecyclerView.Adapter {
 
@@ -62,7 +67,7 @@ public class MultiTypesAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, int position) {
 
         if (holder instanceof MessageViewHolder) {
             ((MessageViewHolder) holder).tvUsername.setText(mArrayUsername[rnd.nextInt(7)]);
@@ -70,6 +75,18 @@ public class MultiTypesAdapter extends RecyclerView.Adapter {
         } else if (holder instanceof LandscapeViewHolder) {
             ((LandscapeViewHolder) holder).landscapeImageView.setImageResource(imgs[rnd.nextInt(7)]);
         }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int itemPosition = holder.getAdapterPosition();
+                Log.d(TAG, "onClick: " + itemPosition);
+                mDataSet.remove(itemPosition);
+                notifyItemRemoved(itemPosition);
+                notifyItemRangeChanged(itemPosition,mDataSet.size());
+            }
+        });
     }
 
     @Override
